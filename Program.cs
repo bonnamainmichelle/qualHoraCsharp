d4ed092af207a5649ebbd90769c549143be4a4ff
Michelle Bonnamain <bonnamainmichelle@gmail.com>
2022-09-21 21:12:20 -0300

    if, else CSharp

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+{
+
+Console.WriteLine("Qual a hora atual?");
+int hora = int.Parse(Console.ReadLine());
+if (hora < 12)
+{
+Console.WriteLine("Bom dia!");
+}
+else if (hora < 18)
+{
+Console.WriteLine("Boa tarde!");
+}
+else
+{
+Console.WriteLine("Boa noite!");
+}
+}
+
